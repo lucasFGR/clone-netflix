d1927919de769ccdff8beabd0d15fced85ad8cbf
Zeer0e1 <lucasfreitasr@outlook.com>
2022-12-14 09:19:18 -0300

    Finalizando projeto

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "3a2f1a601df95da311207418210a8bc0";
+
+let img_url = "https://image.tmdb.org/t/p/w500/";
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
